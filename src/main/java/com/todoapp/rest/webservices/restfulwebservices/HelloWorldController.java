ae965623e148ad7bce8c3e2335a742cf074c9d66
Abhishek Kumar Aman <abhishekaman3015@gmail.com>
2021-06-08 11:37:34 +0530

    Added Path variable

--- src/main/java/com/todoapp/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/todoapp/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.todoapp.rest.webservices.restfulwebservices;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +16,13 @@ public class HelloWorldController {
 		return "Hello World";
 	}
 
+	@GetMapping(path = "/hellobean")
 	public HelloWorldBean helloWorldBean() {
 		return new HelloWorldBean("Hello World Bean");
-	}
+	} 
+
+	@GetMapping(path = "/hellobean/pathvariable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello , %s", name));
+	} 
 }
