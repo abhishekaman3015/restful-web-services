e96bd92ee94361a70d6066fa20ed14ec8f85563e
Abhishek Kumar Aman <abhishekaman3015@gmail.com>
2021-06-09 14:13:17 +0530

    Connected backend to react frontend CrossOrigin

--- src/main/java/com/todoapp/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/todoapp/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.todoapp.rest.webservices.restfulwebservices;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class HelloWorldController {
 	
 
